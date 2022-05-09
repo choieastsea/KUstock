ae413f8fe9410cbcd54012d2795a20fe56c6ea88
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-05-09 17:12:46 +0900

    Update assist.py

--- server/django-rest-framework/myapp/assist.py
+++ server/django-rest-framework/myapp/assist.py
@@ -90,6 +90,7 @@ class assist:
                     req_user = req_user.first().uname
                 else:
                     success = "사용자가 존재하지않습니다.\n"
+                    req_user = ""
         else:
             success = "/community rank 혹은 /community <user> 형태로 입력되었는지 확인해주세요.\n"
 
