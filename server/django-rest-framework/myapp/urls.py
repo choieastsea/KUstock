30036ae5e5c17b41945ee95353ecd32f943fd145
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-05-11 20:19:33 +0900

    Update urls.py

--- server/django-rest-framework/myapp/urls.py
+++ server/django-rest-framework/myapp/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('help',views.help), # /help 명령어 수행
     path('create',views.createUser),
     path('trade', views.trade), # /trade 명령어 수행
-    path('community', views.community), # /trade 명령어 수행
+    path('community', views.community), # /community 명령어 수행
+    path('stock',views.stock),   # /stock 명령어 수행
+    path('chart',views.chart),   # /chart 명령어 수행
     path('check', views.check),  #김혁진 추가 나중에 빼야됨.
     path('dbInit', views.dbInit)  #김혁진 추가 나중에 빼야됨.
 ]
