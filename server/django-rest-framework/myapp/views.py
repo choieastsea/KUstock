27e1b312fabc896bafc111127400b5ce22d2725f
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-05-11 20:39:09 +0900

    Update views.py
    stock 함수에 파싱테스트 문 추가

--- server/django-rest-framework/myapp/views.py
+++ server/django-rest-framework/myapp/views.py
@@ -61,7 +61,8 @@ def createUser(request):
 def stock(request):
     # msg에서 명령어 파싱
     [success, stock_code, theme] = assist.parseStock(request.GET['msg'])
-
+    # 파싱 테스트
+    print("success:"+ success+", stock_code : "+str(stock_code)+", theme:"+ str(theme))
     return_string = ""
 
     if success == "deal":
