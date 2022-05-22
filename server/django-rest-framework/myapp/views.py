197715f2814e8caa3f6aafa16fb7464320cf0bfb
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-05-22 19:22:11 +0900

    /help 명령어 간단한 수정

--- server/django-rest-framework/myapp/views.py
+++ server/django-rest-framework/myapp/views.py
@@ -542,28 +542,33 @@ def community(request):
 
 def help(request):
     req_str = request.GET['msg'].split(' ')
+    uname = request.GET["id"]
     if len(req_str) == 1:
         return JsonResponse({
             "data" : "명령어는 /로 시작하며 앞으로 나오는 "+
             "&lt;,&gt; 는 실제로 입력하지 않고 사용합니다.\n"+
-            "help &lt;function/term&gt; : 'function'의 명령어를 출력합니다."+
-            "'term'의 용어가 해당하는 의미/사용가능한 문자열을 출력합니다.\n"+
-            "community : 사용자 랭킹 및 자산정보관련 명령어입니다.\n"+
+            "kustock : trade와 community에 필요한 정보를 생성하는 명령어입니다. \n"+
             "trade : 주식으로 사고/팔때 사용하는 명령어입니다.\n"+
+            "community : 사용자 랭킹 및 자산정보관련 명령어입니다.\n"+
             "stock : 주식관련 정보를 요청하는 명령어입니다.\n"+
             "chart : 주식의 차트를 요청하는 명령어입니다.\n"+
-            "alarm : 지정한 시각에 알림을 설정하는 명령어입니다.\n"
+            "alarm : 지정한 시각에 알림을 설정하는 명령어입니다.\n"+
+            "상세한 명령어의 정보가 필요하시면 아래와 같이 help 명령어를 입력해주세요.\n"+
+            "help &lt;function/term&gt; : 'function'의 명령어의 상세 사용법을 출력합니다."+
+            "'term'의 용어가 해당하는 의미/사용가능한 문자열을 출력합니다.\n"
         })
     else:
         if req_str[1] == "trade":
             return JsonResponse({
                 "data" : "trad buy &lt;stock&gt; &lt;count&gt; : \"stock\"에 해당하는 주식을 현재가로 \"count\"만큼 매수 요청\n"+
-                "trade buy &lt;stock&gt; &lt;count&gt; : \"stock\"에 해당하는 주식을 현재가로 \"count\"만큼 매도 요청\n"
+                "trade buy &lt;stock&gt; &lt;count&gt; : \"stock\"에 해당하는 주식을 현재가로 \"count\"만큼 매도 요청\n"+
+                "ex) /trade buy 삼성전자 10 => 삼성전자 주식 10개 구매\n /trade sell 삼성전자 10 => 삼성전자 주식 10개 판매\n"
             })
         elif req_str[1] == "community":
             return JsonResponse({
                 "data" : "community rank : 전체 사용자 수익률 랭킹 출력\n"+
-                "community &lt;user&gt; : 특정 사용자 보유 종목 및  수익률 출력\n"
+                "community &lt;user&gt; : 특정 사용자 보유 종목 및  수익률 출력\n"+
+                "ex) /community rank => 전체 사용자 랭킹 출력\n /community "+uname+" => "+uname+"님의 보유 종목 및 수익률 출력"
             })
         elif req_str[1] == "stock":
             return JsonResponse({
@@ -571,7 +576,9 @@ def help(request):
                 "stock top sum : 거래대금이 가장 높은 종목 5개를 출력\n"+
                 "stock top rise : 상승률이 가장 높은 종목 5개를 출력\n" +
                 "stock theme &lt;theme&gt; : \"theme\"에 해당하는 테마 정보 출력\n"+
-                "stock state &lt;stock&gt; : \"stock\"에 해당하는 기업의 재무제표 출력\n"
+                "stock state &lt;stock&gt; : \"stock\"에 해당하는 기업의 재무제표 출력\n"+
+                "ex) /stock top deal \n /stock state 삼성전자 => 삼성전자의 재무제표 출력\n"+
+                " /stock theme 2차전지 => 2차전지 관련 테마주의 정보 출력"
             })
         elif req_str[1] == "chart":
             return JsonResponse({
@@ -579,7 +586,9 @@ def help(request):
                 "chart &lt;stock&gt; inform : 해당 주식의 가격, 거래량, 등락률 출력\n"+
                 "chart &lt;stock&gt; institutional : 기관의 순매매량 출력\n"+
                 "chart &lt;stock&gt; individual : 개인의 순매매량 출력\n"+
-                "chart &lt;stock&gt; foreign : 외국인의 순매매량 출력\n"
+                "chart &lt;stock&gt; foreign : 외국인의 순매매량 출력\n"+
+                "ex) /chart 삼성전자 => 삼성전자 주식의 차트 출력\n"+
+                " /chart 삼성전자 inform => 삼성전자 주식의 가격,거래량,등락률 출력\n"
             })
         elif req_str[1] == "alarm":
             return JsonResponse({
