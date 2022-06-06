2a9f3d2a0e8e79cad761a7d58fdd2065056e8555
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-06-06 14:59:28 +0900

    Update test.js
    쓸데없는 코드 지움

--- r/test.js
+++ r/test.js
@@ -13,25 +13,7 @@ var map = new Map([
   ["/record", true],
   ["/easy", true]
 ]);
-
-/**
- * (string) room
- * (string) sender
- * (boolean) isGroupChat
- * (void) replier.reply(message)
- * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
- * (string) imageDB.getProfileBase64()
- * (string) packageName
- */
-function response(
-  room,
-  msg,
-  sender,
-  isGroupChat,
-  replier,
-  imageDB,
-  packageName
-) {
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   var check = msg.trim()[0];
   if(check=="/"){
     var order = msg.split(" ")[0];
