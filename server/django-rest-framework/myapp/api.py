b7343fd0aff0d118accc5cf1ab63a8050d18db06
choieastsea <choieastsea@gmail.com>
2022-06-11 22:46:36 +0900

    Rollback

--- server/django-rest-framework/myapp/api.py
+++ server/django-rest-framework/myapp/api.py
@@ -1,12 +1,15 @@
+
+
+
+
 """
 주식 시세 조회를 위한 api 요청 등을 처리하고, views.py에서 원하는 결과에 맞게 응답하는 함수를 포함한다.
 """
 from myproject.settings import get_env_variable
 from bs4 import BeautifulSoup
 import requests
-import random
 
-def getStockPriceTest(code):
+def getStockPrice(code):
     url = "https://finance.naver.com/item/main.naver?code=" +str(code)
     print(url)
     headers = {'User-agent': 'Mozilla/5.0'}
@@ -17,37 +20,3 @@ def getStockPriceTest(code):
     print(tr)
     in_str = int(tr[0].text.replace(",", ""))
     return in_str
-
-def getStockPrice(code):
-    """
-    this function is for test
-    """
-    #rootURL = "https://api.odcloud.kr/api/GetStockSecuritiesInfoService/v1/"
-    #url = rootURL + f"getStockPriceInfo?resultType=json&likeSrtnCd={code}&basDt=20220602&serviceKey=9OdEbSYuSWsY3x8Jk%2Bm%2FbFKeOKKPfY6olRpGAUQ8QVVC3xgfbEq8NdZvwscyJTv0KpH2TJIX3E3YylLo%2BUntsA%3D%3D"
-    #res = requests.get(url).json()
-    # print(res)
-    url = "https://finance.naver.com/item/sise.naver?code=" +str(code[1:])
-    headers = {'User-agent': 'Mozilla/5.0'}
-    res = requests.get(url, headers=headers)
-    html = res.content
-    soup = BeautifulSoup(html, 'html.parser')
-    tr = soup.select('#content > div.section.inner_sub > div:nth-child(1) > table > tbody > tr:nth-child(5) > td:nth-child(4)')
-    high_price = int(tr[0].text.replace(",", ""))
-    tr = soup.select('#content > div.section.inner_sub > div:nth-child(1) > table > tbody > tr:nth-child(6) > td:nth-child(4)')    
-    low_price = int(tr[0].text.replace(",", ""))
-
-    print(high_price, low_price)
-    # 100000원 이상은 500원 ,50000원 이상은 100원, 10000원 이상은 50원, 1000원 이상은 10원, 100원 이상은 5원
-    unit = 0
-    if high_price > 100000:
-        unit = 500
-    elif high_price >50000:
-        unit = 100
-    elif high_price > 10000:
-        unit = 50
-    elif high_price > 5000:
-        unit = 10
-    else:
-        unit = 5
-    print(high_price, low_price, unit)
-    return random.randint(0,int((high_price-low_price)/unit))*unit + low_price
